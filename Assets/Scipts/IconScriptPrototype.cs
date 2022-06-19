232132a4351c71354a9c886aa352885c9da8c9a8
WilliamBrett <William.brett@yahoo.com>
2022-06-19 16:20:22 -0500

    Asset Polishing
    Most icons have been modified, additional changes to facilitate demo have been made.

--- Assets/Scipts/IconScriptPrototype.cs
+++ Assets/Scipts/IconScriptPrototype.cs
@@ -2,22 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class IconScriptPrototype : MonoBehaviour
 {
     public Image anImage;
     public Sprite anIcon;
+    //[SerializeField] private GameObject TextContainer;
+    public GameObject TextContainer;
+    private TextMeshProUGUI textMeshPro;
+    public GameObject ankh;
 
-    // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
+        if (TextContainer != null)
+        {
+            textMeshPro = TextContainer.GetComponent<TextMeshProUGUI>();
+        }
 
     }
-
-    // Update is called once per frame
-    void Update()
+    public void dotheankhthing()
     {
-
+        if (textMeshPro != null)
+        {
+            textMeshPro.text += "The inverted ankh is used to denote that a actor is 'dead'.";
+        }
+        ankh.SetActive(true);
     }
 
     public void ImageSwap()
